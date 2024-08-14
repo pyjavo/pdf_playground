7b690952df3447eb50b1fc6aa72298c15475655f
pyjavo <javierdazacomas@gmail.com>
2024-08-14 14:02:05 -0500

    feat: made code of decrypt a single PDF file a function

--- decrypt_pdfs.py
+++ decrypt_pdfs.py
@@ -2,40 +2,46 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 from decouple import config
 
 
-# Create a PdfFileWriter object
-out = PdfFileWriter()
 
-# Open encrypted PDF file with the PdfFileReader
-file = PdfFileReader("inputs/mi_archivo.pdf")
+def pdf_decrypter(path):
+    # Create a PdfFileWriter object
+    out = PdfFileWriter()
 
-# Store correct password in a variable password.
-password = config('PASSWORD')
+    # Open encrypted PDF file with the PdfFileReader
+    file = PdfFileReader(path)
 
-# Check if the opened file is actually Encrypted
-if file.isEncrypted:
+    # Store correct password in a variable password.
+    password = config('PASSWORD')
 
-	# If encrypted, decrypt it with the password
-	file.decrypt(password)
+    # Check if the opened file is actually Encrypted
+    if file.isEncrypted:
 
-	# Now, the file has been unlocked.
-	# Iterate through every page of the file
-	# and add it to our new file.
-	for idx in range(file.numPages):
-		# Get the page at index idx
-		page = file.getPage(idx)
+        # If encrypted, decrypt it with the password
+        file.decrypt(password)
 
-		# Add it to the output file
-		out.addPage(page)
+        # Now, the file has been unlocked.
+        # Iterate through every page of the file
+        # and add it to our new file.
+        for idx in range(file.numPages):
+            # Get the page at index idx
+            page = file.getPage(idx)
 
-	# Open a new file "myfile_decrypted.pdf"
-	with open("outputs/mi_archivo_decrypted.pdf", "wb") as f:
+            # Add it to the output file
+            out.addPage(page)
 
-		# Write our decrypted PDF to this file
-		out.write(f)
-	# Print success message when Done
-	print("File decrypted Successfully.")
+        # Open a new file "myfile_decrypted.pdf"
+        with open("outputs/mi_archivo_decrypted.pdf", "wb") as f:
+            # Write our decrypted PDF to this file
+            out.write(f)
+        # Print success message when Done
+        print("File decrypted Successfully.")
 
-else:
-	# If file is not encrypted, print the
-	# message
-	print("File already decrypted.")
+    else:
+        # If file is not encrypted, print the
+        # message
+        print("File already decrypted.")
+
+if __name__ == '__main__':
+    path = 'inputs/mi_archivo.pdf'
+    pdf_decrypter(path)
+
