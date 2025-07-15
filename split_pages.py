acfc6633002e299a583b5e8938a98c5bf643d1dc
Javier Daza <javierdazacomas@gmail.com>
2025-07-15 08:27:52 -0500

    feat: Filename is an input from user. Remove harcoded string

--- split_pages.py
+++ split_pages.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
@@ -12,12 +13,12 @@ OUTPUT_DIR = 'assets/outputs/'
 def pdf_splitter(filename):
     #filename = os.path.splitext(os.path.basename(path))[0]
 
-    pdf = PdfFileReader(INPUT_DIR+filename)
+    pdf = PdfFileReader(INPUT_DIR+'splits/'+filename)
     for page in range(pdf.getNumPages()):
         pdf_writer = PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
 
-        output_filename = OUTPUT_DIR + f'{filename}_page_{page+1}.pdf'
+        output_filename = OUTPUT_DIR +'splits/' + f'{filename}_page_{page+1}.pdf'
 
         with open(output_filename, 'wb') as out:
             pdf_writer.write(out)
@@ -25,7 +26,10 @@ def pdf_splitter(filename):
         print('Successfully splitted: {}'.format(output_filename))
 
 if __name__ == '__main__':
-    filename = 'certificado.pdf'
-    #path = 'inputs/certificado.pdf'
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <filename.pdf>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
     pdf_splitter(filename)
 
